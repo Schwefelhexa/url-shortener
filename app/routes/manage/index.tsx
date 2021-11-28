daac0ddf9666b6bc121f8d1cb8165f58970458aa
Alexander Baron <alexander.baron1@rwth-aachen.de>
2021-11-28 20:29:35 +0100

    "Create new" link

--- app/routes/manage/index.tsx
+++ app/routes/manage/index.tsx
@@ -1,5 +1,6 @@
 import { Redirect } from ".prisma/client";
 import { LoaderFunction, useLoaderData } from "remix";
+import { PrimaryButton, PrimaryLink } from "~/components/button";
 import Table, { TableBody, TableHead, TableRow } from "~/components/Table";
 import { db } from "~/utils/db.server";
 
@@ -19,21 +20,24 @@ export default function Manage() {
       : "";
 
   return (
-    <Table>
-      <TableHead>
-        <TableHead.Cell>Slug</TableHead.Cell>
-        <TableHead.Cell>Target</TableHead.Cell>
-      </TableHead>
-      <TableBody>
-        {redirects.map((redirect) => (
-          <TableRow key={redirect.id}>
-            <TableRow.Cell highlight>
-              {hostname}/redirect/{redirect.slug}
-            </TableRow.Cell>
-            <TableRow.Cell>{redirect.target}</TableRow.Cell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <main className="space-y-6">
+      <Table>
+        <TableHead>
+          <TableHead.Cell>Slug</TableHead.Cell>
+          <TableHead.Cell>Target</TableHead.Cell>
+        </TableHead>
+        <TableBody>
+          {redirects.map((redirect) => (
+            <TableRow key={redirect.id}>
+              <TableRow.Cell highlight>
+                {hostname}/redirect/{redirect.slug}
+              </TableRow.Cell>
+              <TableRow.Cell>{redirect.target}</TableRow.Cell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <PrimaryLink to="create_redirect">Create new</PrimaryLink>
+    </main>
   );
 }
